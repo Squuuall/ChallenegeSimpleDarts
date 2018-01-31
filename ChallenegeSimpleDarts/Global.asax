175280ffb804fd9e5bef49a6bffae87bbe2ff0e3
Josh <josharmstrongphoto@gmail.com>
2018-01-31 10:25:16 -0800

    Initial commit

---
+++ ChallenegeSimpleDarts/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChallenegeSimpleDarts.Global" Language="C#" %>
---
+++ Darts/Dart.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Darts
+{
+    public class Dart
+    {
+        public int Score { get; set; }
+        private Random _random;
+        public bool IsDouble { get; set; }
+        public bool IsTriple { get; set; }
+
+        public Dart(Random random)
+        {
+            _random = random;
+        }
+
+        public void Throw()
+        {
+            Score = _random.Next(0, 21);
+
+            int multiplier = _random.Next(0, 101);
+            if (multiplier > 95) IsTriple = true;
+            else if (multiplier > 90) IsDouble = true;
+        }
+
+    }
+}
